80444d793fa5c5cddf6ec79333109a0f3911b276
Dave Johnson <dave.al.johnson@gmail.com>
2018-02-10 10:52:02 -0500

    Added project

---
+++ CommonLoggingTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using Common.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommonLoggingTest
+{
+    class Program
+    {
+        private static readonly ILog _Log = LogManager.GetLogger<Program>();
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                _Log.Debug("Test debug");
+                _Log.Error("Test error");
+                _Log.Fatal("Test fatal");
+                _Log.Info("Test info");
+                _Log.Trace("Test trace");
+                _Log.Warn("Test warn");
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
